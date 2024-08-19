610d651d6f2d55fdc1ea50b87534df6f2a8516df
daly.devs@gmail.com <daly.devs@gmail.com>
2024-08-19 16:32:08 +0200

    chore: Update nitro configuration to include 'cloudflare:sockets' in external dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  vite: {
-    build: {
-      rollupOptions: {
-        external: ["pg-native"],
-      },
+
+  nitro: {
+    rollupConfig: {
+      external: ["pg-native", "cloudflare:sockets"],
     },
   },
 });
