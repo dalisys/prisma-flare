94b33f49af8ca0c7de4aaaa9586b490b981e6e37
daly.devs@gmail.com <daly.devs@gmail.com>
2024-08-20 19:27:28 +0200

    refactor: Remove user_schema and related tables from migrations

--- server/database/migrations/0001_yellow_multiple_man.sql
+++
@@ -1,5 +0,0 @@
-CREATE SCHEMA "user_schema";
---> statement-breakpoint
-ALTER TABLE "public"."posts" SET SCHEMA "user_schema";
---> statement-breakpoint
-ALTER TABLE "public"."users" SET SCHEMA "user_schema";
--- server/database/migrations/0002_remarkable_jimmy_woo.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE "user_schema"."posts" SET SCHEMA "public";
---> statement-breakpoint
-ALTER TABLE "user_schema"."users" SET SCHEMA "public";
---> statement-breakpoint
-DROP SCHEMA "user_schema";
